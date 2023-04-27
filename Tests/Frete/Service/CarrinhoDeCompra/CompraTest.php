2112ccd82f9dd2f5f95b2f8093f1da1241103774
Guilherme A Almeida <guilherme.almeida@objective.com.br>
2023-04-27 11:13:06 -0300

    coverage

--- Tests/Frete/Service/CarrinhoDeCompra/CompraTest.php
+++ Tests/Frete/Service/CarrinhoDeCompra/CompraTest.php
@@ -2,10 +2,39 @@
 
 namespace Tests\Frete\Service\CarrinhoDeCompra;
 
+use Mockery;
+use PHPUnit\Framework\Attributes\CoversClass;
 use src\Frete\Service\CarrinhoDeCompra\Compra;
 use PHPUnit\Framework\TestCase;
+use src\Frete\Service\Shipping\Correios;
+use src\Frete\Service\Shipping\ShippingInterface;
+use src\Frete\Service\Shipping\ShippingStrategy;
 
+#[CoversClass(ShippingStrategy::class)]
+#[CoversClass(Correios::class)]
+#[CoversClass(Compra::class)]
 class CompraTest extends TestCase
 {
+    public function testCompraSemFrete()
+    {
+        $shipp = Mockery::mock(ShippingInterface::class);
 
+        $this->assertEquals((new Compra($shipp))->calculoDaCompra(101), 101);
+    }
+
+    public function testCompraComFrete()
+    {
+        $shipp = Mockery::mock(ShippingInterface::class)
+            ->shouldReceive('calculaFrete')
+            ->andReturn(1)
+            ->once()
+            ->getMock();
+
+//        $correios = new Correios();
+//        $correios->definiCriterioDeEntrega("123", 10);
+
+        $compra = (new Compra($shipp))->calculoDaCompra(99);
+
+        $this->assertEquals($compra, 100);
+    }
 }
--- Tests/Frete/Service/Correios/CorreiosTest.php
+++ Tests/Frete/Service/Correios/CorreiosTest.php
@@ -2,16 +2,74 @@
 
 namespace Tests\Frete\Service\Correios;
 
+use Mockery;
 use PHPUnit\Framework\Attributes\CoversClass;
-use src\Frete\Service\Correios\Correios;
+use src\Frete\Entidades\Carrinho;
+use src\Frete\Entidades\Produto;
+use src\Frete\Entidades\Usuario;
+use src\Frete\Service\Shipping\Correios;
+use src\Frete\Service\Shipping\ShippingInterface;
+use src\Frete\Service\Shipping\ShippingStrategy;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(Correios::class)]
 class CorreiosTest extends TestCase
 {
+    protected function setUp():void
+    {
+        $this->usuario = new Usuario();
+
+        $this->carrinho = new Carrinho();
+
+        $this->produto = new Produto();
+    }
+
     public function testFrete()
     {
-        $frete = new Correios();
-        $this->assertEquals($frete->calculaFrete(),1);
+        $frete = new ShippingStrategy(new Correios());
+        $this->assertEquals($frete->calculaFrete("123", 123),3);
+    }
+
+    public function testValorDoFrete()
+    {
+        $this->usuario->nome = "Jhon";
+        $this->usuario->cep = "123456978";
+
+        $correios = new Correios();
+        $correios->definiCriterioDeEntrega($this->usuario->cep, 123);
+
+
+        $valor = Mockery::mock(ShippingInterface::class)
+            ->shouldReceive('calculaFrete')
+            ->andReturn(246)
+            ->once()
+            ->getMock();
+
+        $valor = (new ShippingStrategy($valor))->calculaFrete();
+
+        $this->assertEquals($valor, 246);
+    }
+
+    public function atestCompraSemFrete()
+    {
+        $this->produto->nome = "prod1";
+        $this->produto->valor = 10;
+
+        $this->usuario->nome = "Jhon";
+        $this->usuario->cep = "123456978";
+        $this->carrinho->usuario = $this->usuario;
+        $this->carrinho->insertCompraCarrinho($this->produto->getProduto());
+
+        $correios = new Correios();
+        $correios->definiCriterioDeEntrega($this->usuario->cep, 123);
+
+
+        $valor = Mockery::mock(ShippingInterface::class)
+            ->shouldReceive('calculaFrete')
+            ->andReturn(246)
+            ->once()
+            ->getMock();
+
+        $valor = (new ShippingStrategy($valor))->calculaFrete();
     }
 }
